53eb18f4cd4c5be4bb2d4e9073a60c646bd14469
GabePalla <Gabriel_palla@outlook.com>
2024-02-05 11:36:17 -0300

    Adjust in the terminal section style.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     const input = document.getElementsByClassName("textarea-terminal");
     const p = document.getElementById("text-area");
 
-    p.innerHTML = p.innerHTML + input.item(0).value + "<br>";
+    p.innerHTML = p.innerHTML + "USER-0858> " + input.item(0).value + "<br> is not recognized as an internal or external command, operable program or batch file. <br><br>";
  }
 
  testeTerminal()
