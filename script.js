d799a469c2e0f0943770821d7ebdc766406f36cc
GabePalla <Gabriel_palla@outlook.com>
2024-02-11 15:58:26 -0300

    gridGenerator implementation.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 import "./js/terminal.js";
 import "./js/weather-data.js";
 
+
+
+
+function gridGenerator() {
+    const globalContentContainer = document.getElementById('teste');
+    const gridWidth = ((globalContentContainer.getBoundingClientRect().width * 0.13) / 5);
+    globalContentContainer.style.gridTemplateColumns = `repeat(auto-fill, minmax(${gridWidth}px, 1fr))`
+    globalContentContainer.style.gridTemplateRows = `repeat(auto-fill, minmax(${gridWidth}px, 1fr))`
+}
+
+gridGenerator();
