4bb66ce8e4263220511fe08a7319ba066940650b
GabePalla <Gabriel_palla@outlook.com>
2024-03-10 17:38:07 -0300

    removal of code

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 import "./js/terminal.js";
 import "./js/weather-data.js";
-
-function gridGenerator() {
-    const globalContentContainer = document.getElementById('teste');
-    const gridWidth = ((globalContentContainer.getBoundingClientRect().width * 0.11) / 5);
-    globalContentContainer.style.gridTemplateColumns = `repeat(auto-fill, minmax(${gridWidth}px, 1fr))`
-    globalContentContainer.style.gridTemplateRows = `repeat(auto-fill, minmax(${gridWidth}px, 1fr))`
-}
-
-// gridGenerator();
